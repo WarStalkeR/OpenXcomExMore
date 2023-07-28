27aa745a2f86625738479dfea6af8a377b2eba41
WarStalkeR <stalker6000@hotmail.com>
2023-07-28 15:08:45 +0300

    OXCEM init commit

--- src/version.h
+++ src/version.h
@@ -18,11 +18,11 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_ENGINE "Extended"
-#define OPENXCOM_VERSION_SHORT "Extended 7.9.8"
+#define OPENXCOM_VERSION_ENGINE "Extended More"
+#define OPENXCOM_VERSION_SHORT "Extended More 7.9.8"
 #define OPENXCOM_VERSION_LONG "7.9.8.0"
 #define OPENXCOM_VERSION_NUMBER 7,9,8,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2023-07-03)"
+#define OPENXCOM_VERSION_GIT " (v2023-07-28)"
 #endif
