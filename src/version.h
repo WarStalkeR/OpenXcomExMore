c1df12e85583a0cf20ea575748b6f6d1aeeae29e
WarStalkeR <stalker6000@hotmail.com>
2023-07-28 16:31:13 +0300

    ufopaedia 3x3 rooms preview added

--- src/version.h
+++ src/version.h
@@ -18,8 +18,8 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_ENGINE "Extended More"
-#define OPENXCOM_VERSION_SHORT "Extended More 7.9.8"
+#define OPENXCOM_VERSION_ENGINE "Extended"
+#define OPENXCOM_VERSION_SHORT "Extended 7.9.8 + More 1.0.0"
 #define OPENXCOM_VERSION_LONG "7.9.8.0"
 #define OPENXCOM_VERSION_NUMBER 7,9,8,0
 
