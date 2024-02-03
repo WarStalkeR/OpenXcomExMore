566fc24be8f5aaa4b66c1e8704a59d74991d3606
WarStalkeR <stalker6000@hotmail.com>
2024-02-03 21:35:15 +0200

    Feature: multi-craft hangar mechanics implementation

--- src/Basescape/DismantleFacilityState.cpp
+++ src/Basescape/DismantleFacilityState.cpp
@@ -215,6 +215,7 @@ void DismantleFacilityState::btnOkClick(Action *)
 				delete _fac;
 				// Reset the basescape view in case new facilities were created by removing the old one
 				_view->setBase(_base);
+				_base->syncCraftChanges();
 				if (Options::allowBuildingQueue) _view->reCalcQueuedBuildings();
 				break;
 			}
