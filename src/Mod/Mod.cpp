bb13e98f848bed9dd9cd0b91976e4029b47f6f28
WarStalkeR <stalker6000@hotmail.com>
2024-02-03 17:24:45 +0200

    Feature: advanced craft vs hunter killer dogfight behavior

--- src/Mod/Mod.cpp
+++ src/Mod/Mod.cpp
@@ -411,6 +411,8 @@ Mod::Mod() :
 	_manaEnabled(false), _manaBattleUI(false), _manaTrainingPrimary(false), _manaTrainingSecondary(false), _manaReplenishAfterMission(true),
 	_loseMoney("loseGame"), _loseRating("loseGame"), _loseDefeat("loseGame"),
 	_ufoGlancingHitThreshold(0), _ufoBeamWidthParameter(1000),
+	_accelerationPenaltyStandoff(10), _accelerationPenaltyCautious(10), _accelerationPenaltyCombat(10), _accelerationPenaltyManeuver(10),
+	_accelerationCoefficientStandoff(20, 10), _accelerationCoefficientCautious(35, 15), _accelerationCoefficientCombat(50, 20), _accelerationCoefficientManeuver(70, 25),
 	_escortRange(20), _drawEnemyRadarCircles(1), _escortsJoinFightAgainstHK(true), _hunterKillerFastRetarget(true),
 	_crewEmergencyEvacuationSurvivalChance(100), _pilotsEmergencyEvacuationSurvivalChance(100),
 	_soldiersPerRank({-1, -1, 5, 11, 23, 30}),
@@ -3246,6 +3248,14 @@ void Mod::loadFile(const FileMap::FileRecord &filerec, ModScript &parsers)
 	{
 		_flagByKills = doc["flagByKills"].as<std::vector<int> >(_flagByKills);
 	}
+	_accelerationPenaltyStandoff = doc["accelerationPenaltyStandoff"].as<int>(_accelerationPenaltyStandoff);
+	_accelerationPenaltyCautious = doc["accelerationPenaltyCautious"].as<int>(_accelerationPenaltyCautious);
+	_accelerationPenaltyCombat = doc["accelerationPenaltyCombat"].as<int>(_accelerationPenaltyCombat);
+	_accelerationPenaltyManeuver = doc["accelerationPenaltyManeuver"].as<int>(_accelerationPenaltyManeuver);
+	_accelerationCoefficientStandoff = doc["accelerationCoefficientStandoff"].as<std::pair<int, int>>(_accelerationCoefficientStandoff);
+	_accelerationCoefficientCautious = doc["accelerationCoefficientCautious"].as<std::pair<int, int>>(_accelerationCoefficientCautious);
+	_accelerationCoefficientCombat = doc["accelerationCoefficientCombat"].as<std::pair<int, int>>(_accelerationCoefficientCombat);
+	_accelerationCoefficientManeuver = doc["accelerationCoefficientManeuver"].as<std::pair<int, int>>(_accelerationCoefficientManeuver);
 
 
 
