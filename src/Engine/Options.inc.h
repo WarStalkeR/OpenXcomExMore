b28d4898649f26ee2ce1818e39ee7e2d3da587f7
WarStalkeR <stalker6000@hotmail.com>
2024-02-03 23:57:05 +0200

    Feature: Game Data Viewer mode for Tech Tree Viewer

--- src/Engine/Options.inc.h
+++ src/Engine/Options.inc.h
@@ -80,6 +80,7 @@ OPT bool oxceHighlightNewTopics;
 OPT bool oxcePediaShowClipSize;
 OPT bool oxceForce32bitMode;
 OPT bool oxceStartingBaseSelection;
+OPT bool oxceShowAdvancedDebugOptions;
 
 OPT int oxceInterceptTableSize;
 OPT bool oxceEnableSlackingIndicator;
@@ -113,6 +114,12 @@ OPT bool oxceAutomaticPromotions;
 OPT bool oxceEnableOffCentreShooting;
 OPT bool oxceUniformShootingSpread;
 OPT int oxceCrashedOrLanded;
+
+// OXCE hidden, but can be shown
+OPT bool oxceTechTreeDataView;
+OPT int oxceDataViewStringTrunc;
+OPT bool oxceDataViewCleanReplace;
+OPT bool oxceDataViewStrTranslate;
 
 // OXCE hidden, accessible only via options.cfg
 /**
