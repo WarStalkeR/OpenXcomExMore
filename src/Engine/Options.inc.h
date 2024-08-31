08d2ab71b2a15873e1769f7745a97458ec691803
WarStalkeR <stalker6000@hotmail.com>
2024-08-31 08:35:23 +0300

    Feature: base sets and New Game starting base selection

--- src/Engine/Options.inc.h
+++ src/Engine/Options.inc.h
@@ -103,6 +103,7 @@ OPT bool oxceAutoSell;
 OPT bool oxceAutomaticPromotions;
 OPT bool oxceEnableOffCentreShooting;
 OPT int oxceCrashedOrLanded;
+OPT bool oxceStartingBaseSelection;
 
 // OXCE hidden, accessible only via options.cfg
 /**
