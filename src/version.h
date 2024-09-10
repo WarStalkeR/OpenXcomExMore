4eb5eff2b1b7e1df2ed7ae34f260ffd6d4c71c3f
WarStalkeR <stalker6000@hotmail.com>
2024-09-10 08:53:53 +0300

    typo fix

--- src/version.h
+++ src/version.h
@@ -19,7 +19,7 @@
  */
 
 #define OPENXCOM_VERSION_ENGINE "Extended"
-#define OPENXCOM_VERSION_SHORT "Extended 7.14.1 + More (WIP)"
+#define OPENXCOM_VERSION_SHORT "Extended 7.14.1 + More/WIP"
 #define OPENXCOM_VERSION_LONG "7.14.1.0"
 #define OPENXCOM_VERSION_NUMBER 7,14,1,0
 
