675e121793556789736276477605174a97ab640d
WarStalkeR <stalker6000@hotmail.com>
2024-09-11 12:33:18 +0300

    Feature: facility rule flag for alternative construction sprite

--- src/Ufopaedia/StatsForNerdsState.cpp
+++ src/Ufopaedia/StatsForNerdsState.cpp
@@ -3248,6 +3248,7 @@ void StatsForNerdsState::initFacilityList()
 		addSpriteResourcePath(ss, mod, "BASEBITS.PCK", facilityRule->getSpriteFacility());
 
 		addBoolean(ss, facilityRule->getSpriteEnabledRaw(), "spriteEnabled");
+		addBoolean(ss, facilityRule->useAltBuildSprite(), "altBuildSprite");
 		addBoolean(ss, facilityRule->connectorsDisabled(), "connectorsDisabled");
 
 		addSection("{Sounds}", "", _white);
