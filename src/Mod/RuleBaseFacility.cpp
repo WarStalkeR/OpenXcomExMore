9dc440be6267764417efcf018d51f693d6fb56d4
WarStalkeR <stalker6000@hotmail.com>
2024-09-11 12:33:18 +0300

    Feature: facility rule flag for alternative construction sprite

--- src/Mod/RuleBaseFacility.cpp
+++ src/Mod/RuleBaseFacility.cpp
@@ -42,7 +42,7 @@ RuleBaseFacility::RuleBaseFacility(const std::string &type, int listOrder) :
 	_lift(false), _hyper(false), _mind(false), _grav(false), _mindPower(1),
 	_sizeX(1), _sizeY(1), _buildCost(0), _refundValue(0), _buildTime(0), _monthlyCost(0),
 	_storage(0), _personnel(0), _aliens(0), _crafts(0), _labs(0), _workshops(0), _psiLabs(0),
-	_spriteEnabled(false), _craftsHidden(false), _craftOptions(), _optionGroups(),
+	_spriteEnabled(false), _altBuildSprite(false), _craftsHidden(false), _craftOptions(), _optionGroups(),
 	_sightRange(0), _sightChance(0), _radarRange(0), _radarChance(0),
 	_defense(0), _hitRatio(0), _fireSound(0), _hitSound(0), _placeSound(-1), _ammoMax(0), _rearmRate(1), _ammoNeeded(1), _listOrder(listOrder),
 	_trainingRooms(0), _maxAllowedPerBase(0), _sickBayAbsoluteBonus(0.0f), _sickBayRelativeBonus(0.0f),
@@ -108,6 +108,7 @@ void RuleBaseFacility::load(const YAML::Node &node, Mod *mod)
 	_psiLabs = node["psiLabs"].as<int>(_psiLabs);
 
 	_spriteEnabled = node["spriteEnabled"].as<bool>(_spriteEnabled);
+	_altBuildSprite = node["altBuildSprite"].as<bool>(_altBuildSprite);
 	_craftsHidden = node["craftsHidden"].as<bool>(_craftsHidden);
 	_craftOptions = node["craftOptions"].as<std::vector<CraftOption>>(_craftOptions);
 	_optionGroups = node["optionGroups"].as<std::vector<int>>(_optionGroups);
