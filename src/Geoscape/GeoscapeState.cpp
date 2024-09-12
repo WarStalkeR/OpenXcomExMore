de4ecc0b95366473dd08a5ea410ce62b2b660994
WarStalkeR <stalker6000@hotmail.com>
2024-09-12 22:58:17 +0300

    Feature: base function triggers for geoscape scripts

--- src/Geoscape/GeoscapeState.cpp
+++ src/Geoscape/GeoscapeState.cpp
@@ -3846,6 +3846,17 @@ void GeoscapeState::determineAlienMissions()
 				}
 				if (triggerHappy)
 				{
+					// base functions requirements
+					for (auto& triggerBaseFunc : arcScript->getBaseFunctionTriggers())
+					{
+						triggerHappy = (save->isBaseFunctionEnabled(triggerBaseFunc.first,
+							_game->getMod()) == triggerBaseFunc.second);
+						if (!triggerHappy)
+							break;
+					}
+				}
+				if (triggerHappy)
+				{
 					// soldier type requirements
 					for (auto& triggerSoldierType : arcScript->getSoldierTypeTriggers())
 					{
@@ -4041,6 +4052,17 @@ void GeoscapeState::determineAlienMissions()
 			}
 			if (triggerHappy)
 			{
+				// base functions requirements
+				for (auto& triggerBaseFunc : command->getBaseFunctionTriggers())
+				{
+					triggerHappy = (save->isBaseFunctionEnabled(triggerBaseFunc.first,
+						_game->getMod()) == triggerBaseFunc.second);
+					if (!triggerHappy)
+						break;
+				}
+			}
+			if (triggerHappy)
+			{
 				// soldier type requirements
 				for (auto& triggerSoldierType : command->getSoldierTypeTriggers())
 				{
@@ -4222,6 +4244,17 @@ void GeoscapeState::determineAlienMissions()
 					for (auto& triggerFacility : eventScript->getFacilityTriggers())
 					{
 						triggerHappy = (save->isFacilityBuilt(triggerFacility.first) == triggerFacility.second);
+						if (!triggerHappy)
+							break;
+					}
+				}
+				if (triggerHappy)
+				{
+					// base functions requirements
+					for (auto& triggerBaseFunc : eventScript->getBaseFunctionTriggers())
+					{
+						triggerHappy = (save->isBaseFunctionEnabled(triggerBaseFunc.first,
+							_game->getMod()) == triggerBaseFunc.second);
 						if (!triggerHappy)
 							break;
 					}
--- src/Mod/RuleMissionScript.cpp
+++ src/Mod/RuleMissionScript.cpp
@@ -111,6 +111,7 @@ void RuleMissionScript::load(const YAML::YamlNodeReader& node)
 	reader.tryRead("researchTriggers", _researchTriggers);
 	reader.tryRead("itemTriggers", _itemTriggers);
 	reader.tryRead("facilityTriggers", _facilityTriggers);
+	reader.tryRead("baseFunctionTriggers", _baseFunctionTriggers);
 	reader.tryRead("soldierTypeTriggers", _soldierTypeTriggers);
 	reader.tryRead("xcomBaseInRegionTriggers", _xcomBaseInRegionTriggers);
 	reader.tryRead("xcomBaseInCountryTriggers", _xcomBaseInCountryTriggers);
@@ -262,6 +263,14 @@ const std::map<std::string, bool> &RuleMissionScript::getItemTriggers() const
 const std::map<std::string, bool> &RuleMissionScript::getFacilityTriggers() const
 {
 	return _facilityTriggers;
+}
+
+/**
+ * @return a list of base function triggers that govern execution of this script.
+ */
+const std::map<std::string, bool> &RuleMissionScript::getBaseFunctionTriggers() const
+{
+	return _baseFunctionTriggers;
 }
 
 /**
